1ac692f76fbc389a37610d0917132a77f20902e4
xshuky <itsjuneforwork@gmail.com>
2024-02-06 15:34:02 +0100

    add PlayerMovement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,14 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    private float m_Move;
-    public float m_Speed;
-    public float m_Jump;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private float m_Speed = 5.0f;  
+    [SerializeField] private float m_JumpHeight = 1.0f;
+    [SerializeField] private float m_TurnSmoothTime = 0.1f;
+
+    [SerializeField] private Transform groundCheckLeft;
+    [SerializeField] private Transform groundCheckRight;
+
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +22,24 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    
+
+    private void FixedUpdate()
     {
-        m_Move = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        float horizontalMovement = Input.GetAxisRaw("Horizontal") * m_Speed * Time.deltaTime;
 
-        rb.velocity = new Vector2(m_Move * m_Speed, rb.velocity.y);
+        Move(horizontalMovement);   
+    }
 
-        if(Input.GetButtonDown("Jump"))
+    void Move(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, m_TurnSmoothTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.AddForce(new Vector2(rb.velocity.x, m_Jump * 10));
+            rb.AddForce(new Vector2(rb.velocity.x, m_JumpHeight * 10));
         }
     }
 }
