813ccf9a3639e5b8a7276d6552243a0c497c8085
xshuky <itsjuneforwork@gmail.com>
2024-02-06 17:06:08 +0100

    add followed camera

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float timeOffset;
+    public Vector3 posOffset;
+
+    private Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
+    }
+}
