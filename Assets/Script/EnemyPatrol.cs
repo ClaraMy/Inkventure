3719fde49d8d315db529b67210a8663d6c31e330
xshuky <itsjuneforwork@gmail.com>
2024-02-08 14:33:57 +0100

    add enemy pig1

---
+++ Assets/Script/EnemyPatrol.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private float m_speed = 2.0f;
+    [SerializeField] public Transform[] waypoints;
+
+    public SpriteRenderer pig1;
+    private Transform target;
+    private int destPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * m_speed * Time.deltaTime, Space.World);
+
+        // Si l'ennemi est presque arrivé à sa destination
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            pig1.flipX = !pig1.flipX;
+        }
+    }
+}
