dfe9bcc105618730270e268406a557e72d44ebbe
xshuky <itsjuneforwork@gmail.com>
2024-02-09 13:15:21 +0100

    add menu and first build

--- Assets/Script/GameOverManager.cs
+++ Assets/Script/GameOverManager.cs
@@ -47,7 +47,8 @@ public class GameOverManager : MonoBehaviour
 
     public void MainMenuButton()
     {
-
+        DontDestroyOnLoadScene.instance.RemoveFromDontDestroyOnLoad();
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitButton()
