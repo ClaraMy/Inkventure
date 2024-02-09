55230dbbfcb3930280068f26dfec9cea7685902b
xshuky <itsjuneforwork@gmail.com>
2024-02-09 14:15:03 +0100

    add pause menu

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private void Start()
+    {
+        gameIsPaused = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    private void Paused()
+    {
+        // pour afficher le menu pause
+        pauseMenuUI.SetActive(true);
+
+        // pour arrêter le temps
+        Time.timeScale = 0;
+
+        // pour changer le statut du jeu
+        gameIsPaused = true;
+
+        // pour arrêter les mouvements du joueur
+        PlayerMovement.instance.enabled = false;
+    }
+
+    public void Resume()
+    {
+        // pour retirer le menu pause
+        pauseMenuUI.SetActive(false);
+
+        // pour remettre le temps normal
+        Time.timeScale = 1;
+
+        // pour changer le statut du jeu
+        gameIsPaused = false;
+
+        // pour remettre les mouvements du joueur
+        PlayerMovement.instance.enabled = true;
+    }
+
+    public void MainMenu()
+    {
+        DontDestroyOnLoadScene.instance.RemoveFromDontDestroyOnLoad();
+        Resume();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
