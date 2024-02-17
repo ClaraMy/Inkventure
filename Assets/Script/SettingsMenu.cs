c4ec0055b367de3e727e6f88491c770da8162033
xshuky <itsjuneforwork@gmail.com>
2024-02-17 13:41:35 +0100

    add clear data button

--- Assets/Script/SettingsMenu.cs
+++ Assets/Script/SettingsMenu.cs
@@ -55,4 +55,9 @@ public class SettingsMenu : MonoBehaviour
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
+
+    public void ClearSavedData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
 }
