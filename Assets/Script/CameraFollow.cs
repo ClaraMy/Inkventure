3135b4236c3bd8f7d1f19f545e191d457b0f2ddf
xshuky <itsjuneforwork@gmail.com>
2024-02-25 12:58:42 +0100

    edit camera script function position x

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -1,3 +1,5 @@
+using UnityEditor;
+using UnityEditor.XR.LegacyInputHelpers;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -5,17 +7,34 @@ public class CameraFollow : MonoBehaviour
     public GameObject player;
     public float timeOffset;
     public Vector3 posOffset;
+    public float cameraOffsetX = 2f;
 
+    private float targetX;
     private Vector3 velocity;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    private void Start()
+    {
+        targetX = posOffset.x + cameraOffsetX;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
+        if (player != null)
+        {
+            float horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+            if (horizontalMovement > 0)
+            {
+                targetX = posOffset.x + cameraOffsetX;
+            }
+            else if (horizontalMovement < 0)
+            {
+                targetX = posOffset.x - cameraOffsetX;
+            }
+
+            Vector3 targetPosition = new Vector3(targetX, posOffset.y, posOffset.z);
+            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + targetPosition, ref velocity, timeOffset);
+            
+        }
     }
 }
